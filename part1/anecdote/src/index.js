cdf2980efc62a05526269b3b0b76138f0d3d7bca
Jalaj <Jalaj@mail.com>
2020-09-11 19:44:07 +0530

    finished creating anecdotes app

---
+++ part1/anecdote/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const anecdotes = [
+	'If it hurts, do it more often',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
+const App = (props) => {
+	const [selected, setSelected] = useState(0);
+	const initialState = new Array(anecdotes.length).fill(0);
+	const [votes, setVotes] = useState(initialState);
+
+	const handleAnecdotes = () => {
+		const random = Math.floor(Math.random() * 6);
+		// console.log('handleClick -> random', random);
+		setSelected(random);
+	};
+
+	const handleVotes = () => {
+		let votesCopy = [...votes];
+		votesCopy[selected] += 1;
+		// console.log('handleVotes -> votesCopy', votesCopy);
+		setVotes(votesCopy);
+	};
+
+	const maxVoteIndex = votes.indexOf(Math.max(...votes));
+	// console.log('App -> maxVoteIndex', maxVoteIndex);
+	const anecdoteWithMaxVotes = anecdotes[maxVoteIndex];
+
+	return (
+		<div>
+			<h1>Anecdote of the day</h1>
+			<p>{props.anecdotes[selected]}</p>
+			<p>has {votes[selected]} votes</p>
+			<button onClick={handleVotes}>vote</button>
+			<button onClick={handleAnecdotes}>next anecdote</button>
+			<h1>Anecdote with most votes</h1>
+			<p>{anecdoteWithMaxVotes}</p>
+		</div>
+	);
+};
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
